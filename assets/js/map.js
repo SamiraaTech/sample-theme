094dad53e2d55237341daa9b0c51169395aa4636
aaramiss <31129748+aaramiss@users.noreply.github.com>
2021-02-24 16:46:32 +0330

    first commit

---
+++ assets/js/map.js
@@ -0,0 +1,254 @@
+(function($) { 
+function new_map( $el ) { 
+	var $markers = $el.find('.marker'); 
+	var args = {
+		scrollwheel: false,
+		zoom		: 12,
+		disableDefaultUI: true,
+		center		: new google.maps.LatLng(0, 0),
+		mapTypeId	: google.maps.MapTypeId.ROADMAP,
+		styles:[
+			{
+				"featureType": "water",
+				"elementType": "geometry",
+				"stylers": [
+					{
+						"color": "#e9e9e9"
+					},
+					{
+						"lightness": 17
+					}
+				]
+			},
+			{
+				"featureType": "landscape",
+				"elementType": "geometry",
+				"stylers": [
+					{
+						"color": "#f5f5f5"
+					},
+					{
+						"lightness": 20
+					}
+				]
+			},
+			{
+				"featureType": "road.highway",
+				"elementType": "geometry.fill",
+				"stylers": [
+					{
+						"color": "#ffffff"
+					},
+					{
+						"lightness": 17
+					}
+				]
+			},
+			{
+				"featureType": "road.highway",
+				"elementType": "geometry.stroke",
+				"stylers": [
+					{
+						"color": "#ffffff"
+					},
+					{
+						"lightness": 29
+					},
+					{
+						"weight": 0.2
+					}
+				]
+			},
+			{
+				"featureType": "road.arterial",
+				"elementType": "geometry",
+				"stylers": [
+					{
+						"color": "#ffffff"
+					},
+					{
+						"lightness": 18
+					}
+				]
+			},
+			{
+				"featureType": "road.local",
+				"elementType": "geometry",
+				"stylers": [
+					{
+						"color": "#ffffff"
+					},
+					{
+						"lightness": 16
+					}
+				]
+			},
+			{
+				"featureType": "poi",
+				"elementType": "geometry",
+				"stylers": [
+					{
+						"color": "#f5f5f5"
+					},
+					{
+						"lightness": 21
+					}
+				]
+			},
+			{
+				"featureType": "poi.park",
+				"elementType": "geometry",
+				"stylers": [
+					{
+						"color": "#dedede"
+					},
+					{
+						"lightness": 21
+					}
+				]
+			},
+			{
+				"elementType": "labels.text.stroke",
+				"stylers": [
+					{
+						"visibility": "on"
+					},
+					{
+						"color": "#ffffff"
+					},
+					{
+						"lightness": 16
+					}
+				]
+			},
+			{
+				"elementType": "labels.text.fill",
+				"stylers": [
+					{
+						"saturation": 36
+					},
+					{
+						"color": "#333333"
+					},
+					{
+						"lightness": 40
+					}
+				]
+			},
+			{
+				"elementType": "labels.icon",
+				"stylers": [
+					{
+						"visibility": "off"
+					}
+				]
+			},
+			{
+				"featureType": "transit",
+				"elementType": "geometry",
+				"stylers": [
+					{
+						"color": "#f2f2f2"
+					},
+					{
+						"lightness": 19
+					}
+				]
+			},
+			{
+				"featureType": "administrative",
+				"elementType": "geometry.fill",
+				"stylers": [
+					{
+						"color": "#fefefe"
+					},
+					{
+						"lightness": 20
+					}
+				]
+			},
+			{
+				"featureType": "administrative",
+				"elementType": "geometry.stroke",
+				"stylers": [
+					{
+						"color": "#fefefe"
+					},
+					{
+						"lightness": 17
+					},
+					{
+						"weight": 1.2
+					}
+				]
+			}
+		]
+	}; 	
+	var map = new google.maps.Map( $el[0], args);
+	 
+	map.markers = []; 
+	$markers.each(function(){
+		
+    	add_marker( $(this), map );
+		
+	}); 
+	center_map( map ); 
+	return map;	
+}               
+
+ 
+
+function add_marker( $marker, map ) { 
+	var pinimg = $('.acf-map').attr('data-pinimg');
+	var latlng = new google.maps.LatLng( $marker.attr('data-lat'), $marker.attr('data-lng') ); 
+	var marker = new google.maps.Marker({
+		position	: latlng,
+		map			: map,
+		icon		: pinimg
+		//icon: 'http://localhost/faraz/wp-content/uploads/2017/06/contact-marker.png'
+	}); 
+	map.markers.push( marker ); 
+	if( $marker.html() )
+	{		
+		var infowindow = new google.maps.InfoWindow({
+			content		: $marker.html() 
+		});
+		
+		google.maps.event.addListener(marker, 'click', function() {
+
+			infowindow.open( map, marker );
+
+		});
+	}
+
+}
+ 
+function center_map( map ) { 
+	var bounds = new google.maps.LatLngBounds(); 
+	$.each( map.markers, function( i, marker ){
+
+		var latlng = new google.maps.LatLng( marker.position.lat(), marker.position.lng() );
+
+		bounds.extend( latlng );
+
+	}); 
+	if( map.markers.length == 1 )
+	{ 
+	    map.setCenter( bounds.getCenter() );
+	    map.setZoom( 13 );
+	}
+	else
+	{ 
+		map.fitBounds( bounds );
+	}
+}
+ 
+var map = null;
+
+$(document).ready(function(){
+	$('.acf-map').each(function(){ 
+		map = new_map( $(this) );
+	});
+});
+
+})(jQuery);
